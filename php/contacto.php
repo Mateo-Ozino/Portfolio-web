550eef16545f8782cb91b86a2a80a14a8234ea0e
Mateo-Ozino <mateo.ozino@gmail.com>
2022-06-01 19:51:33 -0300

    Corrección de errores en el formulario y de responsive.

--- php/contacto.php
+++ php/contacto.php
@@ -6,11 +6,30 @@ $mensaje =  $_POST["mensaje"];
 
 $mailHeader = "From:".$nombre."<".$mail.">\r\n";
 
-if ($_POST['submit']) {
-    if (mail($destino, "Contacto desde formulario de portfolio", $mensaje, $mailHeader)) {
-    echo 'El mensaje se ha enviado';
-    } else {
-    echo 'Falló el envio';
-    }
-}
+mail($destino, "Contacto desde formulario de portfolio", $mensaje, $mailHeader) or die("Error al enviar el mensaje");
+echo '
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- CSS PROPIO -->
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Contacto</title>
+</head>
+<body>
+    <div class="envioForm">
+        <h1>Gracias por contactarme!</h1>
+        <p>En breve me pondré en contacto contigo.</p>
+        <button class="btn botonEnvio"><a href="../index.html">Volver al inicio</a></button>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+</html>
+';
 ?>
